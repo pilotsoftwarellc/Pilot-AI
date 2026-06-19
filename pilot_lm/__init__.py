2ac8137f13fca3a9887e60d0f9030d260978a115
pilotsoftwarellc <pilotsoftwarellc@gmail.com>
2026-06-19 12:17:43 -0500

    Sync cleaned local tree

--- pilot_lm/__init__.py
+++ pilot_lm/__init__.py
@@ -1,11 +1,1 @@
-from .model import GPT, GPTConfig
-from .tokenizer import EOS_ID, VOCAB_SIZE, decode_bytes, encode_bytes
-
-__all__ = [
-    "GPT",
-    "GPTConfig",
-    "EOS_ID",
-    "VOCAB_SIZE",
-    "decode_bytes",
-    "encode_bytes",
-]
+"""Pilot AI training helpers."""
